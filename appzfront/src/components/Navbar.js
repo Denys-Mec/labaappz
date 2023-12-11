3004c5008cab8010ea755af872ebf911231a7422
OlenkaSenyk <olena.senyk16@gmail.com>
2023-12-12 01:22:39 +0200

    Adaptive commit

--- appzfront/src/components/Navbar.js
+++ appzfront/src/components/Navbar.js
@@ -1,12 +1,35 @@
 import "../style/navbar.css";
 import "../style/general.css";
+
+import React, { useEffect, useRef, useState } from "react";
 import tippy from 'tippy.js';
-import React, {useEffect, useState} from "react";
-import {Link, useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
     const location = useLocation();
     const [isExpanded, setIsExpanded] = useState(false);
+    const [isShowed, setIsShowed] = useState(true);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+
+    const wrapperRef = useRef(null);
+
+    function useOutsideAlerter(ref) {
+        useEffect(() => {
+            function handleClickOutside(event) {
+                if (ref.current && !ref.current.contains(event.target) && isShowed && isMobile) {
+                    closeMenu();
+                }
+            }
+
+            document.addEventListener("mousedown", handleClickOutside);
+
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref, isShowed, isMobile]);
+    }
+
+    useOutsideAlerter(wrapperRef);
 
     useEffect(() => {
         tippy('#myBtn', {
@@ -17,15 +40,47 @@ const Navbar = () => {
         });
     }, []);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 1024);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const handleToggle = () => {
         setIsExpanded(!isExpanded);
     };
 
+    const showMenu = () => {
+        setIsShowed(!isShowed);
+
+        let nav = document.getElementsByClassName("navigation")[0];
+        nav.style.display = "flex";
+        nav.style.width = "80%";
+        let btn = document.getElementsByClassName("menu-btn")[0];
+        btn.style.display = "none";
+    }
+
+    const closeMenu = () => {
+        setIsShowed(!isShowed);
+
+        let nav = document.getElementsByClassName("navigation")[0];
+        nav.style.display = "none";
+        let btn = document.getElementsByClassName("menu-btn")[0];
+        btn.style.display = "block";
+    }
+
     return (
         <div>
-            <nav className={"navigation"}>
+            <button className={"menu-btn"} onClick={showMenu}></button>
+            <nav ref={wrapperRef} className={"navigation"}>
                 <div className={"avatar-side"}>
-                    <img id="myBtn" className={"avatar"} src="https://as1.ftcdn.net/v2/jpg/01/63/11/70/1000_F_163117064_syJkTuCddASYjvl4WqyRmnuy8cDXpoQY.jpg" alt={"avatar"}/>
+                    <img id="myBtn" className={"avatar"} src="https://as1.ftcdn.net/v2/jpg/01/63/11/70/1000_F_163117064_syJkTuCddASYjvl4WqyRmnuy8cDXpoQY.jpg" alt={"avatar"} />
                     <span>Петрик Ольга</span>
                 </div>
                 <Link className={"nav-item"} to="">
